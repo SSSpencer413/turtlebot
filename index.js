0b45ae06dcac8d3b29739e0b87bc9e8ebe368beb
SSSpencer413 <30614666+SSSpencer413@users.noreply.github.com>
2022-05-15 02:40:28 -0400

    fixed main conflict

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 //SSSpencer413
 
-<<<<<<< HEAD
 const discord = require("./discord/index.js");
 discord.run();
-=======
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]});
->>>>>>> 05b8b0243969474b517620b3f089b52112c57c92
 
 const guilded = require("./guilded/index.js");
 guilded.run();
