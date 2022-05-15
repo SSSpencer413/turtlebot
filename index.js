aa0f5af67cff13af299d69c099014809a469864a
SSSpencer413 <30614666+SSSpencer413@users.noreply.github.com>
2022-05-15 03:32:07 -0400

    express

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ discord.run();
 const guilded = require("./guilded/index.js");
 guilded.run();
 
+const express = require("express");
+const app = express(); 
+
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+})
+
+app.listen(3000, () => {
+    console.log('Listening!');
+})
